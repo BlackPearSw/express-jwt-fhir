5d9870dc0b9baf52acf3ec46bf454036973388b5
allanh <allan.hodkinson@blackpear.com>
2017-03-28 12:41:39 +0100

    Update to use v0.0.10 of jwt-claims-fhir and add extra test to check new functionallity.

--- tests/index-tests.js
+++ tests/index-tests.js
@@ -6,91 +6,185 @@ var SECRET = new Buffer('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 
 var should = require('chai').should();
 
-describe('express-jwt-fhir', function () {
-    var auth = lib({
-        jwt: {
-            requireCredentials: true,
-            secret: SECRET,
-            audience: 'https://fhir.example.net',
-            issuer: 'https://auth.example.net'
-        },
-        fhir: {
-            base: 'https://fhir.example.net/svc/fhir'
-        }
-    });
-
-    var options = {
-        expiresIn: 3600,
-        notBefore: 0,
-        issuer: 'https://auth.example.net'
-    };
-
-    var res;
-    var req;
-    var payload;
-
-    beforeEach(function () {
-        payload = {
-            sub: 'user@example.net',
-            aud: [
-                'https://fhir.example.net',
-                'https://fhir.example2.net'
-            ],
-            fhir_scp: ['*'],
-            fhir_act: ['read:Foo'],
-            jti: uuid.v4()
-        };
-
-        req = {
-            headers: {
-                'authorization': 'Bearer ' + jwt.sign(payload, SECRET, options),
-                'host': 'fhir.example.net'
+describe('express-jwt-fhir', function() {
+    describe('with fixed fhir base url', function () {
+        var auth = lib({
+            jwt: {
+                requireCredentials: true,
+                secret: SECRET,
+                audience: 'https://fhir.example.net',
+                issuer: 'https://auth.example.net'
             },
-            originalUrl: '/svc/fhir/Foo/123',
-            protocol: 'https',
-            method: 'GET'
-        };
-
-        res = {
-            status: 0,
-            headers: {},
-            set: function (key, value) {
-                this.headers[key] = value;
-            },
-            sendStatus: function (status) {
-                this.status = status;
+            fhir: {
+                base: 'https://fhir.example.net/svc/fhir'
             }
+        });
+
+        var options = {
+            expiresIn: 3600,
+            notBefore: 0,
+            issuer: 'https://auth.example.net'
         };
-    });
 
-    it('should be implemented as a function', function () {
-        should.exist(auth);
-        auth.should.be.a('function');
-    });
+        var res;
+        var req;
+        var payload;
 
-    it('should call next()', function (done) {
-        auth(req, res, function (err) {
-            should.not.exist(err);
+        beforeEach(function () {
+            payload = {
+                sub: 'user@example.net',
+                aud: [
+                    'https://fhir.example.net',
+                    'https://fhir.example2.net'
+                ],
+                fhir_scp: ['*'],
+                fhir_act: ['read:Foo'],
+                jti: uuid.v4()
+            };
 
-            done();
+            req = {
+                headers: {
+                    'authorization': 'Bearer ' + jwt.sign(payload, SECRET, options),
+                    'host': 'fhir.example.net'
+                },
+                originalUrl: '/svc/fhir/Foo/123',
+                protocol: 'https',
+                method: 'GET'
+            };
+
+            res = {
+                status: 0,
+                headers: {},
+                set: function (key, value) {
+                    this.headers[key] = value;
+                },
+                sendStatus: function (status) {
+                    this.status = status;
+                }
+            };
+        });
+
+        it('should be implemented as a function', function () {
+            should.exist(auth);
+            auth.should.be.a('function');
+        });
+
+        it('should call next()', function (done) {
+            auth(req, res, function (err) {
+                should.not.exist(err);
+
+                done();
+            });
+        });
+
+        it('should populate req.user', function (done) {
+            auth(req, res, function (err) {
+                should.not.exist(err);
+
+                req.user.should.equal(payload.sub);
+
+                done();
+            });
+        });
+
+        it('should return 403 when action not authorised', function (done) {
+            req.originalUrl = '/svc/fhir/Bar/123';
+            auth(req, res, function (err) {
+                err.status.should.equal(403);
+                done();
+            });
         });
     });
 
-    it('should populate req.user', function (done) {
-        auth(req, res, function (err) {
-            should.not.exist(err);
+    describe('with regex fhir base url', function () {
+        var auth = lib({
+            jwt: {
+                requireCredentials: true,
+                secret: SECRET,
+                audience: 'https://fhir.example.net/svc/Z99999',
+                issuer: 'https://auth.example.net'
+            },
+            fhir: {
+                base: {
+                    regexString: '^https://fhir.example.net/svc/\\w\\d{5}/fhir'
+                }
+            }
+        });
 
-            req.user.should.equal(payload.sub);
+        var options = {
+            expiresIn: 3600,
+            notBefore: 0,
+            issuer: 'https://auth.example.net'
+        };
 
-            done();
+        var res;
+        var req;
+        var payload;
+
+        beforeEach(function () {
+            payload = {
+                sub: 'user@example.net',
+                aud: [
+                    'https://fhir.example.net/svc/Z99999'
+                ],
+                fhir_scp: ['*'],
+                fhir_act: ['read:Foo'],
+                jti: uuid.v4()
+            };
+
+            req = {
+                headers: {
+                    'authorization': 'Bearer ' + jwt.sign(payload, SECRET, options),
+                    'host': 'fhir.example.net'
+                },
+                originalUrl: '/svc/Z99999/fhir/Foo/123',
+                protocol: 'https',
+                method: 'GET'
+            };
+
+            res = {
+                status: 0,
+                headers: {},
+                set: function (key, value) {
+                    this.headers[key] = value;
+                },
+                sendStatus: function (status) {
+                    this.status = status;
+                }
+            };
+        });
+
+        it('should be implemented as a function', function () {
+            should.exist(auth);
+            auth.should.be.a('function');
+        });
+
+        it('should call next()', function (done) {
+            auth(req, res, function (err) {
+                should.not.exist(err);
+
+                done();
+            });
+        });
+
+        it('should populate req.user', function (done) {
+            auth(req, res, function (err) {
+                should.not.exist(err);
+
+                req.user.should.equal(payload.sub);
+
+                done();
+            });
+        });
+
+        it('should return 403 when action not authorised', function (done) {
+            req.originalUrl = '/svc/Z99999/fhir/Bar/123';
+            auth(req, res, function (err) {
+                err.status.should.equal(403);
+                done();
+            });
         });
     });
 
-    it('should return 403 when action not authorised', function (done) {
-        req.originalUrl = '/svc/fhir/Bar/123';
-        auth(req, res, function (err) {
-            err.status.should.equal(403);
-            done();
-        });
-    });
 });
+
